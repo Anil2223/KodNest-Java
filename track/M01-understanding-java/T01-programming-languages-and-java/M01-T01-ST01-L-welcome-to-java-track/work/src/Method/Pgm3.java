efeaf704fa98c8bd6bc50c408700a5a69272e3db
Anil kumar <a.anilkumar2223@gmail.com>
2026-08-11 15:51:19 +0530

    Method 3 and Method 4

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Method/Pgm3.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class Pgm3 {
+    public static void main(String[] args) {
+        int res = add();
+        System.out.println(res);
+    }
+
+    static int add() {
+        int a = 10;
+        int b = 20;
+        int total = a + b;
+        return total;
+    }
+}
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Method/Pgm4.java
@@ -0,0 +1,13 @@
+package Method;
+
+public class Pgm4 {
+    public static void main(String[] args) {
+        int res = add(10, 40);
+        System.out.println(res);
+    }
+
+    static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
