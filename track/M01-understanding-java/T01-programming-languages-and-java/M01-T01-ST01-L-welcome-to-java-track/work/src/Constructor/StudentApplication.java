c58a3bd30487b65134a05399dd2664e1a1759475
Anil kumar <a.anilkumar2223@gmail.com>
2026-08-14 15:28:57 +0530

    class default constructor & special method

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Constructor/StudentApplication.java
@@ -0,0 +1,8 @@
+package Constructor;
+
+public class StudentApplication {
+    public static void main(String[] args) {
+        Student s1 = new Student("Anil kumar", 25, 5.6);
+        s1.display();
+    }
+}
