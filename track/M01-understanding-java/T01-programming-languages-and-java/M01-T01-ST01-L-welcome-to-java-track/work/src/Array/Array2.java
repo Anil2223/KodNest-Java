fec2e4f0234722e0c90f4278ddcd20ace2da75a5
Anil kumar <a.anilkumar2223@gmail.com>
2026-08-17 16:35:40 +0530

    Array element assigned  and making reference

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Array2.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Array2.java
@@ -16,5 +16,32 @@ public class Array2 {
             System.out.println(arr[i]);
         }
 
+        int b[] = new int[arr.length];
+        int j = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            b[j] = arr[i];
+            j++;
+        }
+        System.out.println("B array element output ");
+        for (j = 0; j <= b.length - 1; j++) {
+            System.out.println(b[j]);
+        }
+        j = b.length - 1;
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            b[j] = arr[i];
+            j--;
+        }
+        System.out.println("printing B value as assigned reverse value :");
+        for (int i = 0; i <= b.length - 1; i++) {
+            System.out.println(b[i]);
+        }
+        System.out.println("b value assigned  to c as referrence to poting to value b");
+        int c[] = b;
+        for (int i = 0; i <= c.length - 1; i++) {
+            System.out.println(c[i]);
+        }
+        scanner.close();
+
     }
 }
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Arrpgm3.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Arrpgm3.java
@@ -8,6 +8,5 @@ public class Arrpgm3 {
         for (int x : arr) {
             System.out.println(x);
         }
-
     }
 }
