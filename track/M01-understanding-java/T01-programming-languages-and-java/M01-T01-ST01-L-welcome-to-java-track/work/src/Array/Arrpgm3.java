66e8d41d849cb99960d4ecb76c1473ea4ab6419b
Anil kumar <a.anilkumar2223@gmail.com>
2026-08-17 15:44:53 +0530

    Reading and Printing of array element

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Arrpgm3.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Arrpgm3 {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 40, 50 };
+
+        System.out.println("Array Element :");
+        for (int x : arr) {
+            System.out.println(x);
+        }
+
+    }
+}
