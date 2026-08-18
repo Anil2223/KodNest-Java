a323f946a635ff7e3d877c29f54a56e4c91a1515
Anil kumar <a.anilkumar2223@gmail.com>
2026-08-18 15:51:15 +0530

    Element search in array

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/ArrayElementSearch.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class ArrayElementSearch {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50 };
+        ArraySearch as = new ArraySearch();
+        as.arrsearch(arr, 30);
+    }
+}
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/ArraySearch.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class ArraySearch {
+    public void arrsearch(int[] arr, int key) {
+        for (int i = 0; i <= arr.length; i++) {
+
+        }
+    }
+}
