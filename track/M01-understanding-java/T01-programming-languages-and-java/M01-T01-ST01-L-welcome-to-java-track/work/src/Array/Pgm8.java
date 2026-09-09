6e4b1c8b7587605bda42ce76c0c87386d09d6377
Anil kumar <a.anilkumar2223@gmail.com>
2026-09-09 19:13:29 +0530

    toUpperCase()

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Pgm8.java
@@ -0,0 +1,13 @@
+package Array;
+
+import java.util.Locale;
+
+public class Pgm8 {
+    public static void main(String[] args){
+         String str="java";
+         String str2=str.toUpperCase( );
+         System.out.println(str2);
+         System.out.println(str);
+
+    }
+}
