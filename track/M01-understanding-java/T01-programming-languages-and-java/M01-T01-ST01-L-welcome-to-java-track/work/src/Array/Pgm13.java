945b8cd27f94e60314815c2fee5118b09997fa1d
Anil kumar <a.anilkumar2223@gmail.com>
2026-09-10 20:13:36 +0530

    reverse array using toCharArray then creatingnew char array assing same length add pass it to new array on by one

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Array/Pgm13.java
@@ -0,0 +1,22 @@
+package Array;
+import java.util.*;
+public class Pgm13 {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the string ");
+        String str=scanner.nextLine();
+        char arr[]=str.toCharArray();
+        char newArray[]=new char[arr.length];
+        int j=newArray.length-1;
+        for(int i=0;i<arr.length;i++){
+            newArray[j]=arr[i];
+            j--;
+        }
+
+        String reverse=new String(newArray);
+        System.out.println("original array "+str);
+        System.out.println("new array "+reverse);
+
+
+    }
+}
